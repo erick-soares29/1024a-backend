1df198b9aecf23c67f2ec0b22a6d960210002612
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2026-03-20 12:49:11 -0300

    Código da aula 2026-03-20

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,36 @@
 // Get the client
-import mysql from 'mysql2/promise';
+import mysql, { type RowDataPacket } from 'mysql2/promise';
+
+interface IPessoa extends RowDataPacket{
+  id:number,
+  nome:string,
+}
 
 // Create the connection to database
 const connection = await mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  database: 'luademel',
+  database: 'luademels',
 });
 
 // Using placeholders
 try {
-  const results = await connection.execute('SELECT * FROM pessoa')
-  console.log(results);
+  // const result = 
+  //   await connection
+  //   .execute('INSERT INTO pessoa (id,nome) VALUES (?,?)',[3,"Maria"])
+  // console.log(result)
+
+  const [dados,campos] = await connection.execute<IPessoa[]>('SELECT * FROM pessoa')
+  console.log(dados[0]);
+  for (let i = 0; i < dados.length; i++) {
+    const element = dados[i];
+    console.log(element?.id,element?.nome)
+    
+  }
+
+
 } catch (err) {
+  //TODO:
   console.log(err);
 }
 // Close the connection
