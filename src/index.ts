655f82258aec2f090deb3fdd06a921c4cc16a9be
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2026-03-24 09:25:48 -0300

    Código que lista dados do banco de dados.

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,49 @@
 // Get the client
-import mysql, { type RowDataPacket } from 'mysql2/promise';
+import mysql, { type RowDataPacket, type Connection } from 'mysql2/promise';
 
-interface IPessoa extends RowDataPacket{
-  id:number,
-  nome:string,
+import express from 'express';
+const app = express()
+
+//Como cria uma rota no express?
+app.get("/pessoas", async (req, res) => {
+    let connection: Connection | null = null
+    try {
+        const connection = await mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            database: 'luademel',
+        });
+        // const result = 
+        //   await connection
+        //   .execute('INSERT INTO pessoa (id,nome) VALUES (?,?)',[3,"Maria"])
+        // console.log(result)
+
+        const [dados, campos] = await connection.execute<IPessoa[]>('SELECT * FROM pessoa')
+        res.status(200).json(dados)
+
+        await connection.end();
+    } catch (err) {
+        //TODO:
+        console.log(err);
+        if (connection) {
+            await (connection as Connection).end();
+        }
+    }
+})
+app.post("/pessoas", (req, res) => {
+    //Pegar as informações do usuário   => REQ.body
+    //Conectar com o banco
+    //inserir
+    //Retornar algo que indique que deu certo
+
+})
+app.listen(8000, () => {
+    console.log("Iniciando o servidor na porta 8000")
+})
+
+
+interface IPessoa extends RowDataPacket {
+    id: number,
+    nome: string,
 }
 
-// Create the connection to database
-const connection = await mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'luademels',
-});
-
-// Using placeholders
-try {
-  // const result = 
-  //   await connection
-  //   .execute('INSERT INTO pessoa (id,nome) VALUES (?,?)',[3,"Maria"])
-  // console.log(result)
-
-  const [dados,campos] = await connection.execute<IPessoa[]>('SELECT * FROM pessoa')
-  console.log(dados[0]);
-  for (let i = 0; i < dados.length; i++) {
-    const element = dados[i];
-    console.log(element?.id,element?.nome)
-    
-  }
-
-
-} catch (err) {
-  //TODO:
-  console.log(err);
-}
-// Close the connection
-await connection.end();
